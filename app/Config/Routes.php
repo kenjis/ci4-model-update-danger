3bfa1afd265ba61d3d0198d5dae5223e67c9c43c
kenjis <kenji.uui@gmail.com>
2022-11-15 15:52:09 +0900

    feat: make front page News

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,7 +31,7 @@ $routes->set404Override();
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
+$routes->get('/', [News::class, 'index']);
 
 $routes->match(['get', 'post'], 'news/create', [News::class, 'create']);
 $routes->get('news/edit/(:num)', [News::class, 'edit']);
