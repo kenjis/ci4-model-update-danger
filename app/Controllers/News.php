e24d8881c4e014ff2a2f676464eb6a94c8f6e91e
kenjis <kenji.uui@gmail.com>
2022-11-15 14:49:22 +0900

    feat: add News Controller, Model, Views

---
+++ app/Controllers/News.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\NewsModel;
+use CodeIgniter\Controller;
+use CodeIgniter\Exceptions\PageNotFoundException;
+
+class News extends Controller
+{
+    public function index()
+    {
+        $model = model(NewsModel::class);
+
+        $data = [
+            'news'  => $model->getNews(),
+            'title' => 'News archive',
+        ];
+
+        return view('templates/header', $data)
+            . view('news/overview', $data)
+            . view('templates/footer', $data);
+    }
+
+    public function view($slug = null)
+    {
+        $model = model(NewsModel::class);
+
+        $data['news'] = $model->getNews($slug);
+
+        if (empty($data['news'])) {
+            throw new PageNotFoundException('Cannot find the news item: ' . $slug);
+        }
+
+        $data['title'] = $data['news']['title'];
+
+        return view('templates/header', $data)
+            . view('news/view', $data)
+            . view('templates/footer', $data);
+    }
+
+    public function create()
+    {
+        $model = model(NewsModel::class);
+
+        if ($this->request->getMethod() === 'post' && $this->validate([
+            'title' => 'required|min_length[3]|max_length[255]',
+            'body'  => 'required',
+        ])) {
+            $model->save([
+                'title' => $this->request->getPost('title'),
+                'slug'  => url_title($this->request->getPost('title'), '-', true),
+                'body'  => $this->request->getPost('body'),
+            ]);
+
+            return view('news/success');
+        }
+
+        return view('templates/header', ['title' => 'Create a news item'])
+            . view('news/create')
+            . view('templates/footer');
+    }
+}
---
+++ app/Views/news/create.php
@@ -0,0 +1,15 @@
+<h2><?= esc($title); ?></h2>
+
+<?= validation_list_errors(); ?>
+
+<form action="/news/create" method="post">
+    <?= csrf_field() ?>
+
+    <label for="title">Title</label>
+    <input type="input" name="title" /><br />
+
+    <label for="body">Text</label>
+    <textarea name="body"></textarea><br />
+
+    <input type="submit" name="submit" value="Create news item" />
+</form>
