0e640e8ceb8ac8da95e35d9a0cdfb616d8efe65a
kenjis <kenji.uui@gmail.com>
2022-11-15 14:49:49 +0900

    feat: add routes for News

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -2,6 +2,8 @@
 
 namespace Config;
 
+use App\Controllers\News;
+
 // Create a new instance of our RouteCollection class.
 $routes = Services::routes();
 
@@ -30,6 +32,10 @@ $routes->set404Override();
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+
+$routes->match(['get', 'post'], 'news/create', [News::class, 'create']);
+$routes->get('news/(:segment)', [News::class, 'view']);
+$routes->get('news', [News::class, 'index']);
 
 /*
  * --------------------------------------------------------------------
