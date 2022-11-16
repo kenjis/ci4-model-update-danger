faa19d5e38b3f0fc85275ea7ea2872daf8201958
kenjis <kenji.uui@gmail.com>
2022-11-16 13:51:01 +0900

    feat: add News2, another implementation

---
+++ app/Controllers/News2.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\NewsModel;
+use CodeIgniter\Exceptions\PageNotFoundException;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class News2 extends BaseController
+{
+    private NewsModel $model;
+
+    public function __construct()
+    {
+        $this->model = model(NewsModel::class);
+    }
+
+    public function getIndex(): string
+    {
+        $data = [
+            'news'  => $this->model->getNews(),
+            'title' => 'News archive',
+        ];
+
+        $data['controller'] = strtolower(class_basename(__CLASS__));
+
+        return view('templates/header', $data)
+            . view('news/overview2', $data)
+            . view('templates/footer', $data);
+    }
+
+    public function getView($slug = null): string
+    {
+        $data['news'] = $this->model->getNews($slug);
+
+        if (empty($data['news'])) {
+            throw new PageNotFoundException('Cannot find the news item: ' . $slug);
+        }
+
+        $data['title']      = $data['news']['title'];
+        $data['controller'] = strtolower(class_basename(__CLASS__));
+
+        return view('templates/header', $data)
+            . view('news/view', $data)
+            . view('templates/footer', $data);
+    }
+
+    public function getCreate(): string
+    {
+        helper('form');
+
+        if ($this->request->getMethod() === 'post' && $this->validate([
+            'title' => 'required|min_length[3]|max_length[255]',
+            'body'  => 'required',
+        ])) {
+            $this->model->save([
+                'title' => $this->request->getPost('title'),
+                'slug'  => url_title($this->request->getPost('title'), '-', true),
+                'body'  => $this->request->getPost('body'),
+            ]);
+
+            return view('news/success');
+        }
+
+        $data['controller'] = strtolower(class_basename(__CLASS__));
+
+        return view('templates/header', ['title' => 'Create a news item'])
+            . view('news/create', $data)
+            . view('templates/footer');
+    }
+
+    public function postCreate(): string
+    {
+        return $this->getCreate();
+    }
+
+    public function getEdit($id = ''): string
+    {
+        helper('form');
+
+        if ($id === '') {
+            throw new PageNotFoundException('Cannot find the news item: ' . $id);
+        }
+
+        $data['news'] = $this->model->find($id);
+
+        if (empty($data['news'])) {
+            throw new PageNotFoundException('Cannot find the news item: ' . $id);
+        }
+
+        $data['controller'] = strtolower(class_basename(__CLASS__));
+
+        return view('templates/header', ['title' => 'Edit news item'])
+            . view('news/edit2', $data)
+            . view('templates/footer');
+    }
+
+    /**
+     * @param false|int $id
+     *
+     * @return ResponseInterface|string
+     */
+    public function postUpdate($id = false)
+    {
+        if ($this->request->getMethod() === 'post' && $this->validate([
+            'title' => 'required|min_length[3]|max_length[255]',
+            'body'  => 'required',
+        ])) {
+            $title = $this->request->getVar('title');
+            $slug  = url_title($title, '-', true);
+
+            $data = [
+                'title' => $title,
+                'slug'  => $slug,
+                'body'  => $this->request->getVar('body'),
+            ];
+            $this->model->update($id, $data);
+
+            return $this->response->redirect(site_url('news2/view/' . $slug));
+        }
+
+        return $this->getEdit($id);
+    }
+}
---
+++ app/Views/news/edit2.php
@@ -0,0 +1,15 @@
+<h2><?= esc($title); ?></h2>
+
+<?= validation_list_errors(); ?>
+
+<form action="<?= site_url($controller . '/update/' . $news['id']) ?>" method="post">
+    <?= csrf_field() ?>
+
+    <label for="title">Title</label>
+    <input type="input" name="title" value="<?= esc($news['title'], 'attr') ?>" size="50"><br>
+
+    <label for="body">Text</label>
+    <textarea name="body" cols="80" rows="5"><?= esc($news['body']) ?></textarea><br>
+
+    <input type="submit" name="submit" value="Update news item">
+</form>
